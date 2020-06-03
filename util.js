36f406b96a44de617db9017f753a84d8c7580bf8
ssh <949589855@qq.com>
2020-06-03 15:26:25 +0800

    refactor: remove useless

--- util.js
+++ util.js
@@ -1,9 +1,3 @@
-let bodyWidth = Number(
-  getComputedStyle(document.body).width.replace("px", ""),
-)
-
-let halfBodyWidth = bodyWidth / 2
-
 let preload = (imgs) => {
   return new Promise((resolve, reject) => {
     const length = imgs.length
@@ -25,16 +19,4 @@ let preload = (imgs) => {
     }
     imgs.forEach(load)
   })
-}
-
-let sum = (nums) => nums.reduce((a, b) => a + b, 0)
-
-let omitByIndexes = (arr, omitIndexes) => {
-  let res = []
-  for (let i = 0; i < arr.length; i++) {
-    if (!omitIndexes.includes(i)) {
-      res.push(i)
-    }
-  }
-  return res
 }
